6a13b938a894474f5ef1b825cf1eed72fbe6ea04
Vincent Bernardoff <vb@luminar.eu.org>
2024-12-09 10:19:04 +0100

    deps: use base only

--- lib/bech32.ml
+++ lib/bech32.ml
@@ -2,48 +2,29 @@
    Copyright (c) 2018 Vincent Bernardoff. All rights reserved.
    Distributed under the ISC license, see terms at the end of the file.
   ---------------------------------------------------------------------------*)
-open Rresult
-open Astring
-
-module Int32 = struct
-  include Int32
-
-  external ( ~- ) : t -> t = "%int32_neg"
-
-  (* external (+) : t -> t -> t = "%int32_add"
-   * external (-) : t -> t -> t = "%int32_sub" *)
-  external ( lsl ) : t -> int -> t = "%int32_lsl"
-  external ( lsr ) : t -> int -> t = "%int32_lsr"
-  external ( land ) : t -> t -> t = "%int32_and"
-
-  (* external (lor) : t -> t -> t = "%int32_or" *)
-  external ( lxor ) : t -> t -> t = "%int32_xor"
-
-  let of_char c = c |> Char.to_int |> of_int
-end
+open Base
+open Result.Monad_infix
 
 let convertbits_exn ~pad ~frombits ~tobits data =
   let maxv = (1 lsl tobits) - 1 in
-  let ret, bits, acc =
-    String.fold_left
-      (fun (ret, bits, acc) v ->
-         let v = Char.to_int v in
-         if v < 0 || v lsr frombits <> 0
-         then invalid_arg ("convertbits_exn: invalid data " ^ string_of_int v);
-         let acc = (acc lsl frombits) lor v in
-         let bits = bits + frombits in
-         let rec loop ret bits =
-           if bits >= tobits
-           then (
-             let bits = bits - tobits in
-             loop (((acc lsr bits) land maxv) :: ret) bits)
-           else ret, bits
-         in
-         let ret, bits = loop ret bits in
-         ret, bits, acc)
-      ([], 0, 0)
-      data
+  let foldf =
+    fun (ret, bits, acc) v ->
+    let v = Char.to_int v in
+    if v < 0 || v lsr frombits <> 0
+    then invalid_arg ("convertbits_exn: invalid data " ^ Int.to_string v);
+    let acc = (acc lsl frombits) lor v in
+    let bits = bits + frombits in
+    let rec loop ret bits =
+      if bits >= tobits
+      then (
+        let bits = bits - tobits in
+        loop (((acc lsr bits) land maxv) :: ret) bits)
+      else ret, bits
+    in
+    let ret, bits = loop ret bits in
+    ret, bits, acc
   in
+  let ret, bits, acc = String.fold ~f:foldf ~init:([], 0, 0) data in
   let ret =
     let shift = tobits - bits in
     if pad
@@ -55,13 +36,13 @@ let convertbits_exn ~pad ~frombits ~tobits data =
   let buf = Bytes.create (List.length ret) in
   let _ =
     List.fold_right
-      (fun c i ->
-         Bytes.set buf i (Char.of_byte c);
-         succ i)
+      ~f:(fun c i ->
+        Bytes.set buf i (Char.of_int_exn c);
+        Int.succ i)
       ret
-      0
+      ~init:0
   in
-  Bytes.unsafe_to_string buf
+  Bytes.unsafe_to_string ~no_mutation_while_string_reachable:buf
 ;;
 
 let convertbits ~pad ~frombits ~tobits data =
@@ -214,21 +195,22 @@ let charset_rev =
   |]
 ;;
 
-let invalid_hrp c = c < '\033' || c > '\126' || (c >= 'A' && c <= 'Z')
+let invalid_hrp c = Char.(c < '\033' || c > '\126' || (c >= 'A' && c <= 'Z'))
 
 let check_hrp chk i hrp =
   try
     let chk, i =
-      String.fold_left
-        (fun (a, i) c ->
-           if invalid_hrp c then raise Exit;
-           Int32.(polymod_step a lxor (of_char c lsr 5)), succ i)
-        (chk, i)
+      String.fold
+        ~f:(fun (a, i) c ->
+          if invalid_hrp c then raise Stdlib.Exit;
+          let c = Char.to_int c |> Int32.of_int_exn in
+          Int32.(polymod_step a lxor (c lsr 5)), Int.succ i)
+        ~init:(chk, i)
         hrp
     in
     Ok (chk, i)
   with
-  | Exit -> Error "invalid hrp"
+  | Stdlib.Exit -> Error "invalid hrp"
 ;;
 
 let encode5 ~hrp data =
@@ -240,33 +222,38 @@ let encode5 ~hrp data =
   let chk = polymod_step chk in
   let buf = Bytes.create String.(length hrp + 1 + datalen + 6) in
   let chk, i =
-    String.fold_left
-      (fun (a, i) c ->
-         Bytes.set buf i c;
-         Int32.logxor (polymod_step a) (Char.to_int c land 0x1f |> Int32.of_int), succ i)
-      (chk, 0)
+    String.fold
+      ~f:(fun (a, i) c ->
+        let open Int32 in
+        Bytes.set buf i c;
+        let a = polymod_step a lxor (Int.(Char.to_int c land 0x1f) |> of_int_exn) in
+        a, Int.succ i)
+      ~init:(chk, 0)
       hrp
   in
   Bytes.set buf i '1';
   let chk, i =
-    String.fold_left
-      (fun (a, i) c ->
-         let c = Char.to_int c in
-         Bytes.set buf i charset.[c];
-         Int32.logxor (polymod_step a) (Int32.of_int c), succ i)
-      (chk, succ i)
+    String.fold
+      ~f:(fun (a, i) c ->
+        let c = Char.to_int c in
+        Bytes.set buf i charset.[c];
+        let open Int32 in
+        polymod_step a lxor of_int_exn c, Int.succ i)
+      ~init:(chk, Int.succ i)
       data
   in
-  let chk = List.fold_left (fun a () -> polymod_step a) chk [ (); (); (); (); (); () ] in
+  let chk =
+    List.fold_left ~f:(fun a () -> polymod_step a) ~init:chk [ (); (); (); (); (); () ]
+  in
   let chk = Int32.(chk lxor 1l) in
   let compute_chk chk i =
     let shift = (5 - i) * 5 in
-    Int32.((chk lsr shift) land 0x1fl |> to_int)
+    Int32.((chk lsr shift) land 0x1fl |> to_int_exn)
   in
   for j = 0 to 5 do
     Bytes.set buf (i + j) charset.[compute_chk chk j]
   done;
-  Ok (Bytes.unsafe_to_string buf)
+  Ok (Bytes.unsafe_to_string ~no_mutation_while_string_reachable:buf)
 ;;
 
 let encode ~hrp data = convertbits ~pad:true ~frombits:8 ~tobits:5 data >>= encode5 ~hrp
@@ -275,58 +262,58 @@ let decode bech32 =
   let len = String.length bech32 in
   (if len < 8 || len > 90 then Error "bad input length" else Ok ())
   >>= fun () ->
-  match String.cut ~rev:true bech32 ~sep:"1" with
-  | None -> Error "missing separator"
-  | Some (hrp, payload) ->
-    let hrplen = String.length hrp in
-    let payloadlen = len - 1 - hrplen in
-    (if hrplen < 1 || payloadlen < 6 then Error "bad hrp/payload length" else Ok ())
-    >>= fun () ->
-    let have_upper = ref false in
-    let have_lower = ref false in
-    (try
-       String.fold_left
-         (fun a c ->
-            let cint = Char.(to_int (Ascii.lowercase c)) in
-            if cint < 33 || cint > 126 then raise Exit;
-            if Char.Ascii.is_upper c then have_upper := true;
-            if Char.Ascii.is_lower c then have_lower := true;
-            Int32.logxor (polymod_step a) (cint lsr 5 |> Int32.of_int))
-         1l
-         hrp
-       |> R.ok
-     with
-     | Exit -> Error ("invalid hrp char in " ^ hrp))
-    >>= fun chk ->
-    let chk = polymod_step chk in
-    let chk =
-      String.fold_left
-        (fun a c ->
-           Int32.logxor (polymod_step a) (Char.to_int c land 0x1f |> Int32.of_int))
-        chk
-        hrp
-    in
-    let buf = Bytes.create (payloadlen - 6) in
-    (try
-       String.fold_left
-         (fun (a, i) c ->
-            if Char.Ascii.is_upper c then have_upper := true;
-            if Char.Ascii.is_lower c then have_lower := true;
-            let ci = Char.to_int c in
-            if ci land 0x80 <> 0 then raise Exit;
-            let v = charset_rev.(ci) in
-            if i + 6 < payloadlen then Bytes.set buf i (Char.of_byte v);
-            Int32.logxor (polymod_step a) (Int32.of_int v), succ i)
-         (chk, 0)
-         payload
-       |> fst
-       |> R.ok
-     with
-     | _ -> Error "invalid data")
-    >>= fun chk ->
-    if !have_upper <> !have_lower && chk = 1l
-    then Ok (hrp, Bytes.unsafe_to_string buf)
-    else Error "wrong chksum or mixed case"
+  Result.of_option (String.rsplit2 bech32 ~on:'1') ~error:"no 1 found in address"
+  >>= fun (hrp, payload) ->
+  let hrplen = String.length hrp in
+  let payloadlen = len - 1 - hrplen in
+  (if hrplen < 1 || payloadlen < 6 then Error "bad hrp/payload length" else Ok ())
+  >>= fun () ->
+  let have_upper = ref false in
+  let have_lower = ref false in
+  (try
+     String.fold
+       ~f:(fun a c ->
+         let cint = Char.(to_int (lowercase c)) in
+         if cint < 33 || cint > 126 then raise Stdlib.Exit;
+         if Char.is_uppercase c then have_upper := true;
+         if Char.is_lowercase c then have_lower := true;
+         Int32.(polymod_step a lxor (Int.(cint lsr 5) |> Int32.of_int_exn)))
+       ~init:1l
+       hrp
+     |> Result.return
+   with
+   | Stdlib.Exit -> Error ("invalid hrp char in " ^ hrp))
+  >>= fun chk ->
+  let chk = polymod_step chk in
+  let chk =
+    String.fold
+      ~f:(fun a c ->
+        let open Int32 in
+        polymod_step a lxor (Int.(Char.to_int c land 0x1f) |> Int32.of_int_exn))
+      ~init:chk
+      hrp
+  in
+  let buf = Bytes.create (payloadlen - 6) in
+  (try
+     String.fold
+       ~f:(fun (a, i) c ->
+         if Char.is_uppercase c then have_upper := true;
+         if Char.is_lowercase c then have_lower := true;
+         let ci = Char.to_int c in
+         if ci land 0x80 <> 0 then raise Stdlib.Exit;
+         let v = charset_rev.(ci) in
+         if i + 6 < payloadlen then Bytes.set buf i (Char.of_int_exn v);
+         Int32.(polymod_step a lxor of_int_exn v), Int.succ i)
+       ~init:(chk, 0)
+       payload
+     |> fst
+     |> Result.return
+   with
+   | _ -> Error "invalid data")
+  >>= fun chk ->
+  if Bool.(!have_upper <> !have_lower && Int32.(chk = 1l))
+  then Ok (hrp, Bytes.unsafe_to_string ~no_mutation_while_string_reachable:buf)
+  else Error "wrong chksum or mixed case"
 ;;
 
 module Segwit = struct
@@ -378,19 +365,19 @@ module Segwit = struct
       Bytes.set
         buf
         0
-        (Char.of_byte
+        (Char.of_int_exn
            (match version with
             | 0 -> 0
             | n -> 0x50 + n));
-      Bytes.set buf 1 (Char.of_byte proglen);
-      Bytes.blit_string prog 0 buf 2 proglen;
-      Bytes.unsafe_to_string buf
+      Bytes.set buf 1 (Char.of_int_exn proglen);
+      Bytes.From_string.blit ~src:prog ~src_pos:0 ~dst:buf ~dst_pos:2 ~len:proglen;
+      Bytes.unsafe_to_string ~no_mutation_while_string_reachable:buf
   ;;
 
   let check_version v = if v < 0 || v > 16 then invalid_arg "invalid Segwit version"
 
   let create ?version network prog =
-    Option.iter check_version version;
+    Option.iter ~f:check_version version;
     { network; version; prog }
   ;;
 
@@ -401,13 +388,17 @@ module Segwit = struct
     match version with
     | None ->
       let buf = Bytes.create proglen in
-      Bytes.blit_string prog 0 buf 0 proglen;
-      encode5 ~hrp:N.prefix (Bytes.unsafe_to_string buf)
+      Bytes.From_string.blito ~src:prog ~dst:buf ();
+      encode5
+        ~hrp:N.prefix
+        (Bytes.unsafe_to_string ~no_mutation_while_string_reachable:buf)
     | Some version ->
       let buf = Bytes.create (proglen + 1) in
-      Bytes.set buf 0 (Char.of_byte version);
-      Bytes.blit_string prog 0 buf 1 proglen;
-      encode5 ~hrp:N.prefix (Bytes.unsafe_to_string buf)
+      Bytes.set buf 0 (Char.of_int_exn version);
+      Bytes.From_string.blito ~src:prog ~dst:buf ~dst_pos:1 ();
+      encode5
+        ~hrp:N.prefix
+        (Bytes.unsafe_to_string ~no_mutation_while_string_reachable:buf)
   ;;
 
   let encode_exn t =
@@ -416,7 +407,7 @@ module Segwit = struct
     | Ok v -> v
   ;;
 
-  let pp ppf t = Format.fprintf ppf "%s" (encode_exn t)
+  let pp ppf t = Stdlib.Format.pp_print_string ppf (encode_exn t)
 
   let decode (type a) network addr =
     let module N = (val network : NETWORK with type t = a) in
@@ -425,8 +416,8 @@ module Segwit = struct
     let datalen = String.length data in
     (if datalen < 5 then Error "invalid segwit data" else Ok ())
     >>= fun () ->
-    let hrplow = String.Ascii.lowercase hrp in
-    if hrplow <> N.prefix
+    let hrplow = String.lowercase hrp in
+    if not (String.equal hrplow N.prefix)
     then Error ("invalid segwit hrp " ^ hrp)
     else
       Ok ()
@@ -445,7 +436,7 @@ module Segwit = struct
          else Ok ())
         >>= fun () -> Ok (create network decoded)
       | true ->
-        let decoded = String.(sub data ~start:1 ~stop:datalen |> Sub.to_string) in
+        let decoded = String.sub data ~pos:1 ~len:(datalen - 1) in
         convertbits decoded ~pad:false ~frombits:5 ~tobits:8
         >>= fun decoded ->
         let decodedlen = String.length decoded in
